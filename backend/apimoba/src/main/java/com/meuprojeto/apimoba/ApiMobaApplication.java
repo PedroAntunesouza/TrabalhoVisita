bfd0310c5ed0e35f5b7ff97fae45604826e9558a
pedro <peantunesouza@gmail.com>
2026-05-15 09:35:42 -0300

    mudanças na API

--- backend/apimoba/src/main/java/com/meuprojeto/apimoba/ApiMobaApplication.java
+++ backend/apimoba/src/main/java/com/meuprojeto/apimoba/ApiMobaApplication.java
@@ -7,15 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.TimeZone;
 
 @SpringBootApplication
-public class ApimobaApplication {
+public class ApiMobaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApimobaApplication.class, args);
+		SpringApplication.run(ApiMobaApplication.class, args);
 
 	}
 
 
-	//pro horario no banco nao zoar (3 horas pra tras KAKAKA)
 	@PostConstruct
 	public void init() {
 		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
